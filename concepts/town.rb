1b81fcd27b094a6be828110637478674a0f8e1df
Sage Ross <ragesoss@gmail.com>
2019-03-21 10:06:31 -0700

    Add locations to towns, use those for Overworld

--- concepts/town.rb
+++ concepts/town.rb
@@ -1,10 +1,13 @@
 require_relative './market'
 
+MAP_RANGE = 200..800
+
 class Town
-  attr_accessor :name, :population
-  def initialize name, population: nil
+  attr_accessor :name, :population, :location
+  def initialize name, population: nil, location: nil
     @name = name
     @population = population || rand(2000)
+    @location = location || [rand(MAP_RANGE), rand(MAP_RANGE)]
   end
 
   def key
@@ -21,5 +24,13 @@ class Town
 
   def inn_cost
     @inn_cost ||= rand(5..15)
+  end
+
+  def lat
+    location[0]
+  end
+
+  def long
+    location[1]
   end
 end
--- interfaces/overworld.rb
+++ interfaces/overworld.rb
@@ -4,22 +4,25 @@ class Overworld < Interface
     @background = Gosu::Image.new('media/overworld.png')
     @greeting = Gosu::Image.from_text($adventurer.status, 30)
     @prompt = Gosu::Image.from_text('Where do you want to travel?', 30)
-    @options = {
-      flossvale: '• Flossvale',
-      foo: '• Foo',
-      bar: '• Bar'
-    }
-    @selected_option = @options.keys.index location.key
+    pp location
+    pp towns
+    @selected_option = towns.index location
     setup_input_handling
     set_actions
   end
 
+  TOWN_NAMES = %w[Flossvale Chard Juniper Palmatum Sessa Scond Quercus]
+  def towns
+    @towns ||= TOWN_NAMES.map do |town_name|
+      $state[:towns][town_name.downcase.to_sym] ||= Town.new(town_name)
+    end
+  end
+
   def set_actions
-    @options.keys.each do |town_name|
-      define_singleton_method(town_name) do
-        $state[:towns][town_name] ||= Town.new(town_name.to_s.capitalize)
+   towns.each do |town|
+      define_singleton_method(town.key) do
         destroy
-        InTown.new($state[:towns][town_name]).create
+        InTown.new($state[:towns][town.key]).create
       end
     end
   end
@@ -30,14 +33,18 @@ class Overworld < Interface
   def draw
     @background.draw 0, 0, 0, 0.5, 0.5
     @greeting.draw 10, 10, 0
-    @options.each.with_index do |option, i|
+    towns.each.with_index do |town, i|
       style = @selected_option == i ? { bold: true } : {}
-      Gosu::Image.from_text(option[1], 30, style).draw 50, 160 + 50*i, 1
+      pin = "• #{town.name}"
+      # label
+      Gosu::Image.from_text(pin, 30, style).draw town.lat, town.long, 2
+      # shadow
+      Gosu::Image.from_text(pin, 30, style).draw town.lat + 2, town.long + 2, 1, 1, 1, Gosu::Color.argb(0xff_000000)
     end
   end
 
   def take_action
-    selected_action = @options.keys[@selected_option]
+    selected_action = towns[@selected_option].key
     send selected_action
   end
 
@@ -45,9 +52,9 @@ class Overworld < Interface
     set_button_down do |id|
       case id
       when Gosu::KB_DOWN
-        @selected_option = (@selected_option + 1) % @options.length
+        @selected_option = (@selected_option + 1) % towns.length
       when Gosu::KB_UP
-        @selected_option = (@selected_option - 1) % @options.length
+        @selected_option = (@selected_option - 1) % towns.length
       when Gosu::KB_ENTER, Gosu::KB_RETURN
         take_action
       end
