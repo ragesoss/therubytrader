da73e8344fde8d48be9b3118ead75abcc84e94da
Sage Ross <sage@ragesoss.com>
2019-03-21 21:37:19 -0700

    Add utility for finding the average color of an area of an image
    We can use this to figure out which biome each town is in, and to re-position towns tha are in the ocean.

---
+++ utilities/map_reader.rb
@@ -0,0 +1,26 @@
+require 'rmagick'
+
+class MapReader
+  def initialize file
+    @file = file
+    @image = Magick::Image::read(file).first
+  end
+
+  def patch(row, column)
+    @image.get_pixels(row, column, 3, 3)
+  end
+
+  def average_value(row, column)
+    r = []
+    g = []
+    b = []
+    patch(row, column).each do |pixel|
+      # color => "#5E5E9B9B7E7E"
+      color = pixel.to_color
+      r << color[1..2].hex
+      g << color[5..6].hex
+      b << color[9..10].hex
+    end
+    [r.sum / r.count, g.sum / g.count, b.sum / b.count]
+  end
+end
