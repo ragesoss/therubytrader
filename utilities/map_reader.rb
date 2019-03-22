52e76d991b753fe41fd7ddc11960a6f01e30f861
Sage Ross <sage@ragesoss.com>
2019-03-21 22:04:53 -0700

    Handling for values in the overworld map that are color names

--- utilities/map_reader.rb
+++ utilities/map_reader.rb
@@ -10,13 +10,16 @@ class MapReader
     @image.get_pixels(row, column, 3, 3)
   end
 
+  IGNORE_COLORS = %w[black gray]
   def average_value(row, column)
     r = []
     g = []
     b = []
     patch(row, column).each do |pixel|
-      # color => "#5E5E9B9B7E7E"
+      # color => "#5E5E9B9B7E7E" or "black", etc
       color = pixel.to_color
+      next if IGNORE_COLORS.any? { |name| color.include? name }
+
       r << color[1..2].hex
       g << color[5..6].hex
       b << color[9..10].hex
