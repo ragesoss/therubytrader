7c165ddf411c98c3772a6b97be989a5fe7b91915
Sage Ross <ragesoss@gmail.com>
2019-03-23 14:49:09 -0700

    Use new version of overworld map with corresponding base map
    The base map is now https://www.redblobgames.com/maps/mapgen2/embed.html#seed=195&variant=4&size=huge&noisy-fills=&noisy-edges=true&biomes=true&north-temperature=0.9&south-temperature=-0.75&rainfall=-0.1&persistence=-0.45
    
    The same map gets noise, icons, and lighting added to produce the overworld version, but the base version allows for consistent identification of biome by color, and opens up the door to picking a biome first and then finding a location for it that fits.

--- utilities/town_placer.rb
+++ utilities/town_placer.rb
@@ -4,7 +4,7 @@ class TownPlacer
   MAP_RANGE = 1..2000
 
   def self.map_reader
-    @map_reader ||= MapReader.new 'media/overworld.png'
+    @map_reader ||= MapReader.new 'media/overworld-base.png'
   end
 
   def self.new_location
@@ -38,36 +38,6 @@ class TownPlacer
 
   def self.biome location
     h, s, l = *map_reader.average_value(*location)
-    # mountain: 0, 0, 0
-    # coast : 36, 42, 122
-    # coast:  36, 42, 125
-    # coast: 105, 47, 108
-    # coast: 67, 55, 92
-    # coast: 74, 90, 97
-    # not coast: 93, 37, 118
-    # not coast: 91, 43, 103
-    # not coast: 91, 41, 107
-    # plains: 70, 100, 192
-    # desert: 39, 114, 183
-    # desert: 37, 97, 140
-    # desert: 37, 103, 155
-    # forest: 80, 59, 148
-    # forest: 110, 120, 82
-    # forest: 106, 79, 91
-    # forest: 110, 114, 71
-    # forest: 108, 81, 49
-    # forest : 87, 104, 98
-    # forest: 63, 112, 32
-    # forest: 85, 89, 119
-    # forest : 81, 51, 108
-    # forest: 95, 97, 52
-    # forest river valley: 151, 73, 112
-    # jungle: 147, 119, 60
-    # jungle: 147, 122, 58
-    # swamp: 180, 90, 77
-    # swamp: 180, 95, 71
-    # lake: 210, 128, 100
-    # forest or plains: 83, 92, 48
     if h < 10
       :mountain
     elsif (h > 85) && (h < 96) && (s > 35) && (s < 45) && (l > 100)
